b0dea5553263f7138460020637faa4bfd3a240ee
rgb-katana <komarova1610@yandex.ru>
2022-09-27 14:52:14 +0300

    add some logic with add event listener also add cat images

---
+++ script.js
@@ -0,0 +1,32 @@
+`use strict`;
+
+// variables
+const today = new Date();
+const currentHour = today.getHours();
+const submitButton = document.querySelector(`.fname-button`);
+document.querySelector('.content').classList.add('display');
+document.querySelector('.greeting').classList.add('hidden');
+
+// time-block
+if (currentHour >= 5 && currentHour <= 11) {
+  document.querySelector('.daytime').classList.add('morning');
+} else if (currentHour > 11 && currentHour < 18) {
+  document.querySelector('.daytime').classList.add('day');
+} else if (currentHour >= 18 && currentHour < 5) {
+  document.querySelector('.daytime').classList.add('evening');
+}
+
+// event-listener
+submitButton.addEventListener('click', function () {
+  const userName = document.getElementById('fname').value;
+  document.querySelector('.content').classList.remove('display');
+  document.querySelector('.content').classList.add('hidden');
+  document.querySelector('.greeting').classList.remove('hidden');
+  if (currentHour >= 5 && currentHour <= 11) {
+    document.querySelector('.morning-greet').classList.remove('hidden');
+  } else if (currentHour > 11 && currentHour < 18) {
+    document.querySelector('.day-greet').classList.remove('hidden');
+  } else if (currentHour >= 18 && currentHour < 5) {
+    document.querySelector('.evening-greet').classList.remove('evening');
+  }
+});
